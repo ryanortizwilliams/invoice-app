e100ae3a16abdc81472a5c28fa04d656e9e4271b
ryanortizwilliams <ryanortizwilliams@outlook.com>
2023-11-26 19:09:39 +0900

    Setup project Add routes Add Controllers and Models Add invoice page

---
+++ invoice-app/routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+//use the controllers for api calls
+use App\Http\Controllers\InvoiceController;
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/get_all_invoice', [InvoiceController::class, 'get_all_invoice']);
+
+Route::get('/search_invoice', [InvoiceController::class, 'search_invoice']);
