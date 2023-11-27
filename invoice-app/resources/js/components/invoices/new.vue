30644b66a7ee0c49772b0f3f90847ce0094b98d2
ryanortizwilliams <ryanortizwilliams@outlook.com>
2023-11-27 12:21:53 +0900

    Add subtotal, grandtotal and discount

--- invoice-app/resources/js/components/invoices/new.vue
+++ invoice-app/resources/js/components/invoices/new.vue
@@ -58,6 +58,18 @@ const getproducts = async () => {
     console.log("products", response);
     listproducts.value = response.data.products;
 };
+
+const subtotal = () => {
+    let total = 0;
+    listCart.value.map((data) => {
+        total = total + data.quantity * data.unit_price;
+    });
+    return total;
+};
+
+const grandtotal = () => {
+    return subtotal() - form.value.discount;
+};
 </script>
 <template>
     <div class="container">
@@ -184,20 +196,25 @@ const getproducts = async () => {
                             cols="50"
                             rows="7"
                             class="textarea"
+                            v-model="form.terms_and_conditions"
                         ></textarea>
                     </div>
                     <div>
                         <div class="table__footer--subtotal">
                             <p>Sub Total</p>
-                            <span>$ 1000</span>
+                            <span>$ {{ subtotal() }}</span>
                         </div>
                         <div class="table__footer--discount">
                             <p>Discount</p>
-                            <input type="text" class="input" />
+                            <input
+                                type="text"
+                                class="input"
+                                v-model="form.discount"
+                            />
                         </div>
                         <div class="table__footer--total">
                             <p>Grand Total</p>
-                            <span>$ 1200</span>
+                            <span>$ {{ grandtotal() }}</span>
                         </div>
                     </div>
                 </div>
